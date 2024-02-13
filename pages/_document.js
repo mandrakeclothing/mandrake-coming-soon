a61ad84493a6a94f631704325b9f6184aa45b3e3
Emanuel Christo <emanuelchristochris@gmail.com>
2024-02-14 00:50:12 +0530

    coming soon

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang='en'>
+			<Head>
+				<title>Mandrake™ – Clothing </title>
+				<link rel='icon' type='image/x-icon' href='/favicon.png' />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
